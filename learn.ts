e7f2de80b45ca2b9076e27017158519f2d3221fb
韩硕 <18832643723@163.com>
2018-06-21 08:25:28 +0800

    add code

---
+++ learn.ts
@@ -0,0 +1,33 @@
+/* function sayHello(Person: string) {
+  return 'hello, ' + Person;
+}
+
+let user = [1, 2, 3];
+
+console.log(sayHello(user)); */
+
+/* function alertName(): void {
+  alert('My name is Tom');
+}
+
+let u: undefined = undefined;
+let n: null = null; */
+
+/* let myFavoriteNumber: string | number;
+myFavoriteNumber = '111';
+myFavoriteNumber = 123; */
+
+/* function getLength(something: string | number): string {
+  // return something.length;
+  return something.toString();
+} */
+
+/* interface IPerson {
+  name: string,
+  age: number
+}
+
+let tom: IPerson = {
+  name: 'Tom',
+  age: 18
+}; */
